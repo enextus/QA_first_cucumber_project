70062c9d33b8c027db8e6e986c7d166b35842da7
enextus <enextus@gmail.com>
2021-07-08 09:56:05 +0200

    21_07_08_

--- src/test/java/de/sconto/pages/HomePage.java
+++ src/test/java/de/sconto/pages/HomePage.java
@@ -3,11 +3,23 @@ package de.sconto.pages;
 import de.sconto.util.PropertiesLoader;
 import org.openqa.selenium.By;
 
+import java.util.concurrent.locks.Condition;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selenide.$;
+import static java.util.concurrent.locks.Condition.*;
+
 public class HomePage {
 
     public static String baseURL = PropertiesLoader.loadProperty("url");
-
     private static By acceptBtn = By.xpath("//*[@data-accept-action='all']");
+    private static By acceptBanner = By.cssSelector(".consentForm__root");
 
+    public void acceptCookies() {
+
+        $(acceptBtn).click();
+        $(acceptBanner).shouldNot(exist);
+
+    }
 
 }
